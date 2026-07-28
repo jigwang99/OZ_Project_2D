ed8ff5e9eacb598c1ecc7a880ac4353cc4b5e137
jigwang99 <luca032882@gmail.com>
2026-07-28 17:39:42 +0900

    Entity

---
+++ Assets/01.Scripts/Entity/Rock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] private Vector2 obstacleSize;
+
+    protected void OnEnable()
+    {
+        StartCoroutine(RegisterObtacleNextFrame());
+    }
+    private IEnumerator RegisterObtacleNextFrame()
+    {
+        yield return null;
+        GridManager.instance.UpdateArea(transform.position, obstacleSize);
+    }
+}
