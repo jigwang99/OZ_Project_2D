5127d90ecd279a0d3d09f73d0072ad50fc57f1c5
jigwang99 <luca032882@gmail.com>
2026-07-29 14:39:53 +0900

    Enemy Commander 기능 분리

---
+++ Assets/01.Scripts/Enemy/EnemyContext.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+public enum EnemyPhase
+{
+    Gather,
+    Build,
+    Combat,
+}
+public class EnemyContext
+{
+    public Faction Faction { get; }
+    public Faction PlayerFaction { get; }
+    public Transform Transform { get; }
+
+   public EnemyPhase CurrentPhase { get; set; }
+
+    public EnemyContext(Faction faction, Faction playerFaction, Transform transform)
+    {
+        Faction = faction;
+        PlayerFaction = playerFaction;
+        Transform = transform;
+    }
+    public Vector2 GetCenter()
+    {
+        Castle castle = Castle.FindNearestCastle(Transform.position, FactionType.Enemy);
+        return castle != null ? (Vector2)castle.transform.position : (Vector2)Transform.position;
+    }
+}
---
+++ Assets/01.Scripts/Enemy/EnemyProductionModule.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Serializable]
+public class ProductionRule
+{
+    public UnitType unitType;
+    public int maxCount;
+    public int priority;
+    public List<EnemyPhase> phases = new List<EnemyPhase>();
+}
+
+[Serializable]
+public class ProductionSetting
+{
+    public List<ProductionRule> rules = new List<ProductionRule>();
+}
+public class EnemyProductionModule
+{
+    private readonly EnemyContext enemyContext;
+    private readonly ProductionSetting setting;
+
+    public EnemyProductionModule(EnemyContext enemyContext, ProductionSetting setting)
+    {
+        this.enemyContext = enemyContext;
+        this.setting = setting;
+    }
+    public void Update()
+    {
+        HandleProduction();
+    }
+    private void HandleProduction()
+    {
+        foreach (ProductionRule rule in setting.rules.OrderBy(r => r.priority))
+        {
+            if (!rule.phases.Contains(enemyContext.CurrentPhase))
+                continue;
+
+            int current = enemyContext.Faction.CountUnits(rule.unitType) + QueueCount(rule.unitType);
+            if (current >= rule.maxCount)
+                continue;
+
+            ProductionBuilding building = FindProducer(rule.unitType);
+            if (building == null)
+                continue;
+
+            building.EnqueueUnit(rule.unitType);
+            break;
+        }
+    }
+    private int QueueCount(UnitType type)  //생산큐에있는 유닛 수
+    {
+        int count = 0;
+        foreach (Building building in enemyContext.Faction.Buildings)
+        {
+            if (building is ProductionBuilding pb)
+            {
+                foreach (UnitType queued in pb.ProductList)
+                    if (queued == type)
+                        count++;
+            }
+        }
+        return count;
+    }
+    private ProductionBuilding FindProducer(UnitType type)
+    {
+        ProductionBuilding best = null;
+        int minQueue = int.MaxValue;
+
+        foreach (Building building in enemyContext.Faction.Buildings)
+        {
+            if (!(building is ProductionBuilding productionBuilding) || !productionBuilding.IsAlive)
+                continue;
+            if (productionBuilding.IsQueueFull)
+                continue;
+            if (!productionBuilding.CanProduceType(type))
+                continue;
+
+            if (productionBuilding.ProductList.Count < minQueue)
+            {
+                minQueue = productionBuilding.ProductList.Count;
+                best = productionBuilding;
+            }
+        }
+        return best;
+    }
+}
